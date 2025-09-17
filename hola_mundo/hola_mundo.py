3448c4ee361b6686c54ca11762e34e9ead150e59
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-09-16 23:12:39 -0300

    agrego respuestas

--- hola_mundo/hola_mundo.py
+++ hola_mundo/hola_mundo.py
@@ -1,22 +1,22 @@
 # 1. Imprime "Hola, mundo"
 
-print( tu código aquí )
+print( "Hola, mundo" )
 
 # 2. Imprime "Hola, Valeria" con el nombre en una variable
 
 nombre = "Valeria"
 
-print( tu código aquí ) # con una coma
+print( "Hola", nombre ) # con una coma
 
-print( tu código aquí ) # con un +
+print( "Hola" + nombre ) # con un +
 
 # 3. Imprimir "Hola 156!" con el número en una variable
 
 numero = 156
 
-print( tu código aquí ) # con una coma
+print( "Hola", numero, "!" ) # con una coma
 
-print( tu código aquí ) # con un + -- este debería arrojar un error!, corrígelo con conversión
+print( "Hola" + str(numero) + "!") # con un + -- este debería arrojar un error!, corrígelo con conversión
 
 # 4. Imprimir "Me encanta comer tacos y arepas" con las comidas en variables
 
@@ -24,7 +24,7 @@ comida1 = "tacos"
 
 comida2 = "arepas"
 
-print( tu código aquí ) # con .format()
+print( "Me encanta comer {} y {}".format(comida1, comida2) ) # con .format()
 
-print( tu código aquí ) # con una cadena f
+print( f"Me encanta comer {comida1} y {comida2}" ) # con una cadena f
 
