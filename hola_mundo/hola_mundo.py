392638e0b65a0605878848324b1e92a117bc8fd9
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-09-16 22:56:45 -0300

    agrego codigo ejemplo

---
+++ hola_mundo/hola_mundo.py
@@ -0,0 +1,30 @@
+# 1. Imprime "Hola, mundo"
+
+print( tu código aquí )
+
+# 2. Imprime "Hola, Valeria" con el nombre en una variable
+
+nombre = "Valeria"
+
+print( tu código aquí ) # con una coma
+
+print( tu código aquí ) # con un +
+
+# 3. Imprimir "Hola 156!" con el número en una variable
+
+numero = 156
+
+print( tu código aquí ) # con una coma
+
+print( tu código aquí ) # con un + -- este debería arrojar un error!, corrígelo con conversión
+
+# 4. Imprimir "Me encanta comer tacos y arepas" con las comidas en variables
+
+comida1 = "tacos"
+
+comida2 = "arepas"
+
+print( tu código aquí ) # con .format()
+
+print( tu código aquí ) # con una cadena f
+
